26938cd1bdca2ebb0d3c003db2f96ff7d5984ce8
Tsiuryn_AV <tsiuryn_av@st.by>
2024-05-12 00:13:35 +0300

    some fixes

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -93,7 +93,7 @@ class SettingsPage extends StatelessWidget {
                       },
                     );
                   }),
-                  32.h,
+                  48.h,
                 ],
               ),
             ),
